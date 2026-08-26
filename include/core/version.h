70aa4f64bcaf2a0b96facaefcbdfaa1aac78d709
qianruo <qianruo199704@gmail.com>
2026-08-27 00:39:57 +0800

    modify webui

--- include/core/version.h
+++ include/core/version.h
@@ -2,8 +2,8 @@
 
 #define APP_NAME "rk3588-canfd-and-tcp-ip"
 #define APP_VERSION "1.1.0"
-#define APP_GIT_COMMIT "d3b988b"
+#define APP_GIT_COMMIT "46695a8"
 #define APP_GIT_BRANCH "main"
 #define APP_GIT_DIRTY 0
 #define APP_BUILD_TYPE "Release"
-#define APP_BUILD_DATE "2026-08-26 16:31:44"
+#define APP_BUILD_DATE "2026-08-26 16:36:41"

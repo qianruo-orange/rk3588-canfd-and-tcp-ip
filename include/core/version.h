39e635ce14f62cd67100afc46a3afe63964f4773
qianruo <qianruo199704@email.com>
2026-08-26 15:29:39 +0800

    modify can parament

--- include/core/version.h
+++ include/core/version.h
@@ -4,6 +4,6 @@
 #define APP_VERSION "1.1.0"
 #define APP_GIT_COMMIT "3e3fe97"
 #define APP_GIT_BRANCH "main"
-#define APP_GIT_DIRTY 0
+#define APP_GIT_DIRTY 1
 #define APP_BUILD_TYPE "Release"
-#define APP_BUILD_DATE "2026-08-26 07:11:05"
+#define APP_BUILD_DATE "2026-08-26 07:15:35"
